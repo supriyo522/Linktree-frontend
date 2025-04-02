446f368e2e4ca3083e82a3b5f2467f1cf87b7fb6
supriyo nag <supriyonag522@gmail.com>
2025-04-02 09:47:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Linktree from './Linktree';
+
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Linktree/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Linktree.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Linktree.css";
+
+const links = [
+  { title: "Facebook:raphaaa", url: "https://www.instagram.com/raphaaaofficial?igsh=MWh5YWpzejQ3NA%3D%3D&utm_source=qr" },
+  { title: "Instagram:Citimart", url: "https://www.instagram.com/citimart_official?igsh=MWx1Mnc1aTl2cnFpdg==" },
+  { title: "Facebook:Citimart", url: "https://facebook.com/share/16R31P8msU/?mibextid=wwXlfr" },
+  { title: "Whatsapp", url: "https://web.whatsapp.com/" },
+//   { title: "Blog", url: "https://your-blog.com/" }
+];
+
+const Linktree = () => {
+  return (
+    <div className="container">
+      <div className="linktree-box">
+        <h1 className="title">In house Linktree</h1>
+        <div className="links">
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link"
+            >
+              {link.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Linktree;
